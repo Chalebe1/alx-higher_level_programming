3d5ba5e4d8d7d68ff38bed1fc53de47ccb9e3d6e
chalebe1 <angeloebuks@gmail.com>
2023-01-03 12:56:52 -0800

    task2 done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count = count+1
         except IndexError:
             break
-        except ValueError:
+        except (ValueError,TypeError):
             continue
     print()
     return count
