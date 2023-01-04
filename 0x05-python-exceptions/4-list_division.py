c15b17ddb0fb5355cadf2657bbf082a03b0281f4
chalebe1 <angeloebuks@gmail.com>
2023-01-04 15:22:04 -0800

    task4 done

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except ZeroDivisionError:
             output = 0
-            print("division by zero")
+            print("division by 0")
         finally:
             list.append(output)
     return list
