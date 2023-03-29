00b6712818d890da90b2ee966597684bad53ad03
Chalebe1 <Angeloebuks@gmail.com>
2023-03-29 04:32:43 -0700

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 
 def max_integer(my_list=[]):
     if (my_list):
-        biggest = mylist[0]
+        biggest = my_list[0]
         for i in range(len(my_list)):
             if (my_list[i] > biggest):
                 biggest = my_list[i]
